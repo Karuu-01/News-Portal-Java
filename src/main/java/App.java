0e9abc21c447b744bccc9e410a3ecc2b57c447d3
karuu-01 <brian.muchori@student.moringaschool.com>
2021-08-23 21:51:12 +0300

    make it better

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,7 +10,16 @@ import org.sql2o.Sql2o;
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if(processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
     public static void main (String[] args) {
+        port(getHerokuAssignedPort());
+
         Sql2oDepartmentDao departmentDao;
         Sql2oUsersDao usersDao;
         Sql2oNewsDao newsDao;
